dd556cf083700a279203d3a6c54606ef8bb31222
Ipsita Parida <ipsita.parida@rsa.com>
2021-07-14 04:48:19 -0700

    added react dom

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <h2 className="text-center">
+        Hello React with JSX
+    </h2>,
+    // React.createElement('h2', null, 'Hello React'),
+    document.getElementById('root')
+)
