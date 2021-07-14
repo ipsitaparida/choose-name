e7eaca6f90dc393f0c8db0bffafd78baad75129b
Ipsita Parida <ipsita.parida@rsa.com>
2021-07-14 01:39:18 -0700

    http module

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 import config,{ nodeEnv, logStars} from './config';
-console.log(config, nodeEnv);
-logStars('Function');
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.write('Hello \n');
+    setTimeout(() => {
+        res.write('I can stream!\n');
+        res.end();
+    }, 3000);
+})
+server.listen(8080);
