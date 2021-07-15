00f375226046f5e392f2158aca188cc3ffaa1135
Ipsita Parida <ipsita.parida@rsa.com>
2021-07-15 06:40:15 -0700

    component state

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Header from './Header';
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pageHeader: 'Naming Contests'
+        };
+    }
+    componentDidMount() {
+        console.log("did mount");
+    }
+    componentWillUnmount() {
+        console.log("will mount");
+    }
 
-const App = () => {
-    return (
-        <div className="App">
-            <Header message="Naming Contests" />
-            <div>
-                ...
+    render() {
+        return (
+            <div className="App">
+                <Header message={this.state.pageHeader} />
+                <div>
+                    {this.state.test}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+
+    }
+}
 
 export default App;
