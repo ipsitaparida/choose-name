b51d74cd16e466f816e670b3cae408f5545281cd
Ipsita Parida <ipsita.parida@rsa.com>
2021-07-21 00:05:37 -0700

    reload page of each contest

--- serverRender.js
+++ serverRender.js
@@ -8,14 +8,38 @@ import axios from "axios";
 import config from './config';
 
 
-const serverRender = () => 
-    axios.get(`${config.serverUrl}/api/contests`)
+const getApiUrl = contestId =>{
+    if(contestId){
+        return `${config.serverUrl}/api/contests/${contestId}`;
+    }
+    return `${config.serverUrl}/api/contests`;
+};
+
+const getInitialData = (contestId, apiData) => {
+    console.log(apiData);
+    if(contestId) {
+        return {
+            currentContestId: apiData.id,
+            contests: {
+                [apiData.id]:apiData
+            }
+        }
+    }
+    return{
+        contests:apiData.contests
+    } 
+};
+
+
+const serverRender = (contestId) => 
+    axios.get(getApiUrl(contestId))
     .then(resp => {
+        const initialData = getInitialData(contestId,resp.data);
         return {
             initialMarkup: ReactDOMServer.renderToString(
-            <App initialData={resp.data} />
+            <App initialData={initialData} />
         ),
-        initialData: resp.data
+        initialData
         }
     });
 
